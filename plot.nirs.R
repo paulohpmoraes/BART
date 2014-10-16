a95c83099472bc0af37a027336a4fdae07497cb0
mschutze <mschutze@gmail.com>
2014-10-16 15:49:07 +0100

    Added function to make a 2d plot of up to 128 eeg channels

--- plot.nirs.R
+++ plot.nirs.R
@@ -1,15 +1,16 @@
-#input
+#sample input
 channels <- list(ch1="PO9-PO7", ch2="PO7-O1", ch3="PO7-PO3", ch4="I1-O1", ch5="PO3-O1",
                  ch6="O1-Oz", ch7="PO3-PO2", ch8="POz-Oz", ch9="Oz-Iz", ch10="POz-PO4",
                  ch11="Oz-O2", ch12="PO4-O2", ch13="O2-I2", ch14="PO4-PO8", ch15="O2-PO8",
                  ch16="PO8-PO10")
 betas <- c(0.9, 0.6, 0.75, 0.5, 0.70, 0.78, 0.85, 0.3, 0.22, 0.2, 0.3, 0.15, 0.27, 0.22, 0.32, 0.15)
 
-#test
-plot.nirs(channels,betas)
+#sample plot
+plot.nirs.img(channels,betas)
+plot.nirs.coords(channels,betas)
 
-## function ##
-plot.nirs <- function(channels, betas) {
+## function to plot results on a background image with 64 eeg positions ##
+plot.nirs.img <- function(channels, betas) {
   
   #libraries
   require(png)
@@ -17,12 +18,13 @@ plot.nirs <- function(channels, betas) {
   require(EBImage)
   
   #load table with eeg coordinates
+  #coordinates should be between 0 and 1 with origin on the bottom left corner
   res <- 1000 #resolution of image in pixels
-  ec <- as.data.frame(read.table("eegcoords.txt",header=T))
-  ec[,c(2,3)] <- round(ec[,c(2,3)]*res) #multiply x,y values in ec (between 0 and 1) by res
+  ec <- as.data.frame(read.table("eegcoords64.txt",header=T))
+  ec[,c(2,3)] <- round(ec[,c(2,3)]*res) #multiply x,y values in ec by res
   
   #load background image with eeg coordinates
-  bg.img <- readPNG("eegmap.png",native=F)
+  bg.img <- readPNG("eegmap64.png",native=F)
   bg.matrix <- bg.img[,,1] #only get one channel
   bg.raster <- raster(bg.matrix,xmn=1,xmx=res,ymn=1,ymx=res) #create a raster
   bwpalette <- colorRampPalette(c("black", "white"), bias=1)
@@ -80,4 +82,66 @@ plot.nirs <- function(channels, betas) {
     #locator(1)
   }
 
+}
+
+## function to plot results on a coordinate system with 128 eeg positions ##
+plot.nirs.coords <- function(channels, betas) {
+  
+  #libraries
+  require(raster)
+  require(EBImage)
+  
+  #load table with eeg coordinates
+  #coordinates should be between 0 and 1 with origin at the bottom left corner
+  res <- 1000 #resolution (higher for better smoothing)
+  ec <- as.data.frame(read.table("eegcoords128_2d.txt",header=T))
+  ec[,c(2,3)] <- round(ec[,c(2,3)]*res) #multiply x,y values in ec by res to enable gaussian smoothing
+  
+  #create activation matrix, define color palette, create list with channels
+  usedchannels <- matrix(NA,length(channels)*2,3)
+  colnames(usedchannels) <- c("names","x","y")
+  m <- matrix(0,res,res) #activation matrix
+  colpalette <- colorRampPalette(c("green","yellow","red"), bias=1) #create ramp palette
+  m.colors <- colpalette(255) #sample colors from palette
+  m.colors <- paste(m.colors,substring(rgb(0,0,70,maxColorValue=100),6,7),sep="") #define alpha as HEX (ex: #FF00FF50)
+  
+  #draw a circle in a matrix m centered on coordinates x,y and with radius r and value v
+  drawCircle <- function(m,x,y,r,v) {
+    for(i in -r:r) { j <- round(sqrt(r^2-abs(i)^2)); m[c((x-j):(x+j)),(y+i)] <- v }
+    return(m)
+  }
+  
+  #add values to activation matrix
+  for(k in 1:length(channels)) {
+    ch <- strsplit(channels[[k]],"-")[[1]]
+    #add channel to used channels list
+    usedchannels[k*2-1,] <- c(ch[1],ec[ec$name==ch[1],]$x,ec[ec$name==ch[1],]$y)
+    usedchannels[k*2,] <- c(ch[2],ec[ec$name==ch[2],]$x,ec[ec$name==ch[2],]$y)
+    #calculate middle point px,py between channels
+    x <- c(ec[ec$name==ch[1],]$x, ec[ec$name==ch[2],]$x)
+    y <- c(ec[ec$name==ch[1],]$y, ec[ec$name==ch[2],]$y)
+    if(x[1]>x[2]) { dx <- x[1]-x[2]; px <- x[1]-round(dx/2) } else { dx <- x[2]-x[1]; px <- x[2]-round(dx/2) }
+    if(y[1]>y[2]) { dy <- y[1]-y[2]; py <- y[1]-round(dy/2) } else { dy <- y[2]-y[1]; py <- y[2]-round(dy/2) }
+    d <- sqrt(dx^2+dy^2) #distance d between channels
+    #plot point in matrix m, radius of point correlates to distance between channels
+    m <- drawCircle(m,px,py,d/2.5,betas[k])
+  }
+  
+  #gaussian blur activation matrix and make raster of it
+  m<-t(m[,ncol(m):1])
+  m<-gblur(m, sigma=12)
+  m<-ifelse(m<0.0001,NaN,m)
+  mr<-raster(m,xmn=1,xmx=res,ymn=1,ymx=res)
+  
+  #plot everything in R
+  par(mfrow=c(1,1),mar=c(1,5,1,1),oma=c(0,0,0,0))
+  plot(mr,col=m.colors,axes=F,legend=F,box=F)
+  text(unique(usedchannels)[,2:3],unique(usedchannels)[,1], cex=0.5)
+  
+  #print result in PNG file
+  png("nirscoords.png",height=res,width=res)
+  par(mfrow=c(1,1),mar=c(1,5,1,1),oma=c(0,0,0,0))
+  plot(mr,col=m.colors,axes=F,legend=F,box=F)
+  text(unique(usedchannels)[,2:3],unique(usedchannels)[,1], cex=1.2)
+  dev.off()
 }
